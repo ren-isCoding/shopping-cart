c970b1fdc81b20697196eb9ff822b0fca416cbad
renhubgit <renik2001@gmail.com>
2022-06-18 16:27:38 +0200

    Homepage Main component styling

--- src/components/HomepageComponents/Main.js
+++ src/components/HomepageComponents/Main.js
@@ -6,11 +6,17 @@ export default function Main() {
   return (
     <MainContainer>
       <span className="main-text">BUILD YOUR PC NOW</span>
-      <img src={gif} />
-      <span className="sub-text">Latest and greatest tech.</span>
-      <span className="sub-text">Worldwide fast shipping.</span>
-      <span className="sub-text">Competitive pricing.</span>
-      <button>SHOP NOW</button>
+      <div className="gif-container">
+        <img src={gif} />
+        <a className="shop-btn" href="store">
+          SHOP NOW
+        </a>
+      </div>
+      <div className="sub-div">
+        <span className="sub-text">Latest and greatest tech.</span>
+        <span className="sub-text">Worldwide fast shipping.</span>
+        <span className="sub-text">Competitive pricing.</span>
+      </div>
     </MainContainer>
   )
 }
@@ -21,4 +27,58 @@ const MainContainer = styled.main`
   align-items: center;
   color: whitesmoke;
   width: 50rem;
+
+  img {
+    width: 50rem;
+    margin-bottom: 1rem;
+  }
+
+  .main-text {
+    font-size: 5rem;
+    font-weight: bold;
+  }
+
+  .gif-container {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .shop-btn {
+    position: absolute;
+    color: black;
+    background: whitesmoke;
+    font-weight: 600;
+    letter-spacing: 0.2rem;
+    padding: 0.7rem 2.4rem;
+
+    &::before {
+      position: absolute;
+      border: 0.1rem solid #fff;
+      box-sizing: border-box;
+      content: " ";
+      height: 100%;
+      width: 100%;
+      left: 0;
+      top: 0;
+      transition: 200ms;
+    }
+
+    &:hover::before {
+      transform: translate(0.5rem, 0.4rem);
+    }
+  }
+  .sub-div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .sub-text {
+    font-size: 2.2rem;
+    margin: 0.2rem 0;
+    background: black;
+    padding: 0 1rem;
+  }
 `
