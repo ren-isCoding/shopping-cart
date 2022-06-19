d1538c6268d4b391f451485287c44e580165bd79
renhubgit <renik2001@gmail.com>
2022-06-19 14:08:24 +0200

    added Header to Store Component

---
+++ src/components/StoreComponents/Header.js
@@ -0,0 +1,83 @@
+import React from "react"
+import styled from "styled-components"
+import SearchSVG from "../../assets/svg/Search"
+import ShoppingCartSVG from "../../assets/svg/ShoppingCart"
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <h3>FAKE COMPONENTS</h3>
+      <div className="header-div">
+        <div className="container">
+          <SearchSVG />
+          <span>Search</span>
+        </div>
+        <div className="container">
+          <ShoppingCartSVG />
+          <span>Shopping Cart</span>
+        </div>
+      </div>
+    </HeaderContainer>
+  )
+}
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  width: 80rem;
+  max-height: 10rem;
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+    justify-content: center;
+    gap: 2rem;
+  }
+
+  h3 {
+    letter-spacing: 0.7rem;
+    font-weight: lighter;
+  }
+
+  .header-div {
+    display: flex;
+    justify-content: space-between;
+    gap: 3rem;
+
+    @media (max-width: 800px) {
+      gap: 4rem;
+    }
+  }
+
+  .container {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    padding: 1rem 2rem;
+    padding-top: 0rem;
+    cursor: pointer;
+
+    @media (max-width: 800px) {
+      padding: 0;
+    }
+  }
+
+  .container span {
+    position: relative;
+    font-size: 1.8rem;
+    left: -1rem;
+    opacity: 0;
+    white-space: nowrap;
+    transition: 150ms;
+
+    @media (max-width: 800px) {
+      opacity: 0.9;
+      left: 0;
+    }
+  }
+
+  .container:hover span {
+    left: 0;
+    opacity: 0.9;
+  }
+`
--- src/components/StoreComponents/Store.js
+++ src/components/StoreComponents/Store.js
@@ -1,5 +1,28 @@
 import React from "react"
+import styled from "styled-components"
+import Header from "./Header"
+// import Main from "./Main"
 
 export default function Store() {
-  return <div>Store</div>
+  return (
+    <StoreContainer>
+      <Header />
+      {/* <Main /> */}
+    </StoreContainer>
+  )
 }
+
+const StoreContainer = styled.div`
+  color: #231f20;
+  background: #f8f5f2;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  gap: 12rem;
+  padding-top: 10rem;
+  padding-left: 7rem;
+
+  @media (max-width: 800px) {
+    padding: 10%;
+  }
+`
