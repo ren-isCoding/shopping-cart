bd5361beba1631b8023357ecbd1e8a6d885e3241
renhubgit <renik2001@gmail.com>
2022-06-20 16:03:59 +0200

    changed Router Component name to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import React, { useState } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Homepage from "./components/HomepageComponents/Homepage"
 import Store from "./components/StoreComponents/Store"
 
-export default function Router() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import Router from "./Router"
+import App from "./App"
 import GlobalStyle from "./styles/GlobalStyle"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <Router />
+    <App />
   </React.StrictMode>
 )
