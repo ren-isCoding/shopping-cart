165bce61dbe15d9b73b27a0f34a69827d12a5c14
renhubgit <renik2001@gmail.com>
2022-06-20 16:06:50 +0200

    added PcParts to utils to store pc components

---
+++ src/utils/PcParts.js
@@ -0,0 +1,11 @@
+import img from "../assets/img/CPUs/5600x.webp"
+
+const PcParts = {
+  CPUs: [
+    { name: "AMD Ryzen 5 5600x", price: "299.99€", img: "" },
+    { name: "AMD Ryzen 7 5800x", price: "449.99€", img: "" },
+    { name: "AMD Ryzen 9 5900x", price: "549.99€", img: "" },
+  ],
+}
+
+export default PcParts
